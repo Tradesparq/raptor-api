cca4084683c1c9839e8bb00550e1b4cb34ec63d3
r1n0xmk2 <rene7297152@gmail.com>
2014-09-18 14:12:50 +0800

    close create update destroy

--- routes/market_product.js
+++ routes/market_product.js
@@ -5,10 +5,10 @@ var schema = require('../schema/market_product');
 var rest = new Rest({model: model, schema: schema});
 module.exports = function(app, passport) {
 
-	app.post('/market_product', rest.create.bind(rest));
+	// app.post('/market_product', rest.create.bind(rest));
 	app.get('/market_product/:id', rest.retrieve.bind(rest));
-	app.put('/market_product/:id', rest.update.bind(rest));
-	app.del('/market_product/:id', rest.destroy.bind(rest));
+	// app.put('/market_product/:id', rest.update.bind(rest));
+	// app.del('/market_product/:id', rest.destroy.bind(rest));
 
 	app.get('/market_product', rest.search.bind(rest));
 };
--- routes/market_seller.js
+++ routes/market_seller.js
@@ -5,10 +5,10 @@ var schema = require('../schema/market_seller');
 var rest = new Rest({model: model, schema: schema});
 module.exports = function(app, passport) {
 
-	app.post('/market_seller', rest.create.bind(rest));
+	// app.post('/market_seller', rest.create.bind(rest));
 	app.get('/market_seller/:id', rest.retrieve.bind(rest));
-	app.put('/market_seller/:id', rest.update.bind(rest));
-	app.del('/market_seller/:id', rest.destroy.bind(rest));
+	// app.put('/market_seller/:id', rest.update.bind(rest));
+	// app.del('/market_seller/:id', rest.destroy.bind(rest));
 
 	app.get('/market_seller', rest.search.bind(rest));
 };
